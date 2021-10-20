43f62e474c5186b13517430fc6b7539609e7359c
winterarea <87577641+winterarea@users.noreply.github.com>
2021-10-20 17:41:10 +0800

    Update Binary.java

--- Binary.java
+++ Binary.java
@@ -15,3 +15,42 @@ function read(size) {
     }
     return code;
 }
+readBit:function(buff,pos,size){
+    var i,code=0;
+    var bitCount=buff.length*8;
+    var result=[];
+    while(pos<bitCount){
+        code=0;
+        for(i=0;i<size;i++){
+            if(buff[pos>>3]&1<<(pos&7)){
+                code|=1<<i;
+            }
+            pos++;
+        }
+        result.push(code);
+    }
+    return result;
+},
+writeBit:function(buff,pos,size){
+    var i,j;
+    var resLength=(buff.length*size-pos)>>3;
+    var result=[];
+    //console.log(resLength)
+    var code=0,pos=0;
+    for(i=0;i<buff.length;i++){
+        for(j=0;j<size;j++){
+            if((buff[i]>>j)&1){
+                code|=1<<pos;
+
+            }
+            pos++;
+            //console.log(pos)
+            if(pos>=8){
+                result.push(code);
+                code=0;
+                pos=pos%8;
+            }
+        }
+    }
+    return result;
+},
